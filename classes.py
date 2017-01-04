71a53b2cfd196285d16fc5265926e2304eea71ba
neuhier <basti.hoffmeister@gmx.de>
2017-01-04 20:52:43 +0100

    Now we can run^^

--- classes.py
+++ classes.py
@@ -8,12 +8,10 @@ import sys, pygame, os
 #----------------+
 # Player Class
 #----------------+
-class Player():
+class Player(pygame.sprite.Sprite):
     """Class for the player object."""
 # player attributes
     name = "Jeffrey"    # player Name
-    position = {"x":0,
-                "y":0}  # player's position on map
     angle = 0           # player's viewing direction
     hitpts = 100        # player's life points
     alive = True        # is player alive
@@ -21,20 +19,21 @@ class Player():
 
 # constructor
     def __init__(self, name, hitpts, inventory):
+        pygame.sprite.Sprite.__init__(self)
         self.name = name
         self.hitpts = hitpts
         self.inventory = inventory
+        script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
+        rel_path = "imgs/hero.png"
+        abs_file_path = os.path.join(script_dir, rel_path)
+        self.image = pygame.image.load(abs_file_path).convert()
+        self.rect = self.image.get_rect()
 
-# print method
+    # print method
     def print_player(self):
         print "Player:          " + self.name
         print "Hitpoints:       " + self.hitpts
         print "Position (x/y):  (" + str(self.position["x"]) + "/" + str(self.position["y"]) + ")"
-
-# move player
-    def move(self, xchange, ychange):
-        self.x+=xchange
-        self.y+=ychange
 
 # when player gets hit
     def hurtme(self, dmg):
@@ -46,7 +45,70 @@ class Player():
 
 
 # Start GAME and stuff
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+RED   = (255,   0,   0)
 
+# Initialize Pygame
+pygame.init()
+
+# Set the height and width of the screen
+screen_width = 700
+screen_height = 400
+screen = pygame.display.set_mode([screen_width, screen_height])
+
+block_list = pygame.sprite.Group()
+
+all_sprites_list = pygame.sprite.Group()
+
+# Create a RED player block
+player = Player("Basti", 1000, [])
+all_sprites_list.add(player)
+
+# Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+score = 0
+
+# -------- Main Program Loop -----------
+speed = [0, 0]
+while not done:
+
+
+    player.rect.x += speed[0]
+    player.rect.y += speed[1]
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                speed = [-5, 0]
+            if event.key == pygame.K_RIGHT:
+                speed = [5, 0]
+            if event.key == pygame.K_UP:
+                speed = [0, -5]
+            if event.key == pygame.K_DOWN:
+                speed = [0, 5]
+        if event.type == pygame.KEYUP:
+            speed = [0, 0]
+
+    # Clear the screen
+    screen.fill(BLACK)
+
+    all_sprites_list.draw(screen)
+
+    clock.tick(60)
+
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+pygame.quit()
+
+'''
 pygame.init()
 
 size = width, height = 320, 240
@@ -84,3 +146,4 @@ while 1:
     screen.fill(black)
     screen.blit(hero, ballrect)
     pygame.display.flip()
+'''
